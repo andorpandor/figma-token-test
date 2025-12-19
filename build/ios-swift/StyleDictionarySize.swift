36d67ce13ca27cf17f8ec35037b8b3a75a7a8125
Andreas Qvenild <andreas.qvenild@nrk.no>
2025-12-19 12:56:06 +0100

    update

--- build/ios-swift/StyleDictionarySize.swift
+++ build/ios-swift/StyleDictionarySize.swift
@@ -9,14 +9,21 @@
 import UIKit
 
 public enum StyleDictionarySize {
-    public static let spacing-12 = [object Object]
-    public static let spacing-144 = [object Object]
-    public static let spacing-2 = [object Object]
-    public static let spacing-20 = [object Object]
-    public static let spacing-32 = [object Object]
-    public static let spacing-4 = [object Object]
-    public static let spacing-56 = [object Object]
-    public static let spacing-8 = [object Object]
-    public static let spacing-88 = [object Object]
-    public static let spacing-none = [object Object]
+    public static let dimensions0 = CGFloat(0.00)
+    public static let dimensions1 = CGFloat(64.00)
+    public static let dimensions10 = CGFloat(640.00)
+    public static let dimensions11 = CGFloat(704.00)
+    public static let dimensions12 = CGFloat(768.00)
+    public static let dimensions13 = CGFloat(832.00)
+    public static let dimensions14 = CGFloat(896.00)
+    public static let dimensions15 = CGFloat(960.00)
+    public static let dimensions2 = CGFloat(128.00)
+    public static let dimensions3 = CGFloat(192.00)
+    public static let dimensions4 = CGFloat(256.00)
+    public static let dimensions5 = CGFloat(320.00)
+    public static let dimensions6 = CGFloat(384.00)
+    public static let dimensions7 = CGFloat(448.00)
+    public static let dimensions8 = CGFloat(512.00)
+    public static let dimensions9 = CGFloat(576.00)
+    public static let dimensionsMax = CGFloat(159984.00)
 }
--- build/ios/StyleDictionarySize.m
+++ build/ios/StyleDictionarySize.m
@@ -9,13 +9,20 @@
 #import "StyleDictionarySize.h"
 
 
-float const spacing-none = [object Object];
-float const spacing-2 = [object Object];
-float const spacing-4 = [object Object];
-float const spacing-8 = [object Object];
-float const spacing-12 = [object Object];
-float const spacing-20 = [object Object];
-float const spacing-32 = [object Object];
-float const spacing-56 = [object Object];
-float const spacing-88 = [object Object];
-float const spacing-144 = [object Object];
+float const Dimensions0 = 0.00f;
+float const Dimensions1 = 64.00f;
+float const Dimensions2 = 128.00f;
+float const Dimensions3 = 192.00f;
+float const Dimensions4 = 256.00f;
+float const Dimensions5 = 320.00f;
+float const Dimensions6 = 384.00f;
+float const Dimensions7 = 448.00f;
+float const Dimensions8 = 512.00f;
+float const Dimensions9 = 576.00f;
+float const Dimensions10 = 640.00f;
+float const Dimensions11 = 704.00f;
+float const Dimensions12 = 768.00f;
+float const Dimensions13 = 832.00f;
+float const Dimensions14 = 896.00f;
+float const Dimensions15 = 960.00f;
+float const DimensionsMax = 159984.00f;
